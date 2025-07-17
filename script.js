9b503be1611900e26e467e09a21cf423ca2de325
Diane <dianahporter@gmail.com>
2025-07-17 10:21:27 +0200

    Attached events to  the html elelments

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
+
+//2. Selecting Elements
 function changeAllArticleColors(){
-    let articles = document.querySelectorAll('#products');
-    // articles = 3;
-    for(let char of articles){
-        console.log(char);
-        char.classList.add('sale');
+    const articles = document.querySelectorAll('#products article');
+    
+    for(let article of articles){
+        console.log(article);
+        article.classList.add('sale');
     }
     return articles;
 }
  changeAllArticleColors();
+ 
+ //3. Events
+
+ function attachBuyEvents(){
+    let buttons = document.querySelectorAll('#products button');
+
+    for(const button of buttons){
+        button.addEventListener('click', function(e){
+            console.log("BUY!!")
+        console.log(e.currentTarget );
+        // console.log(this);
+        e.currentTarget.addEventListener('mouseover', function(){
+            document.body.style.backgroundColor = 'lightblue';
+        })
+    })
+    }
+    
+ }
+ attachBuyEvents();
+
+//let button = document.querySelectorAll('#products .buy'); if you want to target the button by using it's class
+
+//4. Attributes
+
 
