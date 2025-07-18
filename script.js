ef104ef4ba35e913a744380e5543906012e7e416
Diane <dianahporter@gmail.com>
2025-07-18 09:27:12 +0200

    attached events in the script

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+/*
 //2. Selecting Elements
 function changeAllArticleColors(){
     const articles = document.querySelectorAll('#products article');
@@ -9,21 +9,39 @@ function changeAllArticleColors(){
     }
     return articles;
 }
- changeAllArticleColors();
+ changeAllArticleColors(); */
  
- //3. Events
+ //3. Events  //4. Attributes
 
  function attachBuyEvents(){
     let buttons = document.querySelectorAll('#products button');
 
     for(const button of buttons){
         button.addEventListener('click', function(e){
-            console.log("BUY!!")
+           /* console.log("BUY!!")
         console.log(e.currentTarget );
         // console.log(this);
         e.currentTarget.addEventListener('mouseover', function(){
             document.body.style.backgroundColor = 'lightblue';
-        })
+        }) */
+       console.log(e.target.parentElement);
+       e.target.parentElement.classList.toggle('sale');
+       console.log(this.parentElement.getAttribute('data-id'));
+    //    const element = document.querySelector('.sale');
+    //    console.log(element.textContent);
+
+       const productArticle = e.target.closest('article');
+       const productName = productArticle.querySelector('h2').textContent;
+       const productPrice = parseInt(productArticle.querySelector('p').textContent);
+       const quantity = parseInt(productArticle.querySelector('input').value);
+       const total = productPrice * quantity;
+      
+       const tfoot = document.querySelector('tfoot');
+       const table = document.querySelector('table');
+       const tbody = document.createElement('tbody');
+       const tr = document.createElement('tr');
+       
+      
     })
     }
     
@@ -32,6 +50,7 @@ function changeAllArticleColors(){
 
 //let button = document.querySelectorAll('#products .buy'); if you want to target the button by using it's class
 
-//4. Attributes
+//5. Creating Elements
+
 
 
